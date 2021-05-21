9aa7580c0d2f2e84e306d1edca15b09e7e5d893f
Jay Daltrey <jaydaltrey@gmail.com>
2021-05-21 04:43:02 +0100

    Add project files.

---
+++ ApprenticeTipsService/Controllers/ApprenticeshipController.cs
@@ -0,0 +1,28 @@
+﻿using ApprenticeTipsService.ApiKeyAuth;
+using ApprenticeTipsService.Classes;
+using ApprenticeTipsService.RequestResponseModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApprenticeTipsService.Controllers
+{ 
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiKeyAuth]
+    public class ApprenticeshipController : ControllerBase
+    {
+        [HttpPost("AddContactDetailsToDB")]
+        public void AddContactDetailsToDB(DetailAdderRequest request)
+        {
+            var DBinserter = new DatabaseInserter();
+
+            int currentId = DBinserter.InsertData("email, firstname, surname, phone, previous_level, comments", $"{request.Contact.EmailAddress}', '{request.Contact.FirstName}', '{request.Contact.Surname}', '{request.Contact.ContactNumber}', '{request.Contact.Qualification}', '{request.Contact.Comment}", "Contact", "webform");
+            DBinserter.InsertCheckboxData(request, currentId);
+
+        }
+    }
+}
